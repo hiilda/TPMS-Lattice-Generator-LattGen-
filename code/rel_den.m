ea6c2f6caae50395ec0a90100e80c34bd5bda128
hiilda <136614281+hiilda@users.noreply.github.com>
2023-09-18 10:40:45 -0600

    Add files via upload

--- code/rel_den.m
+++ code/rel_den.m
@@ -2,7 +2,7 @@ function [isovalue_new,app_t_new] = rel_den(rd_in,imported_mesh,inp,len,bre,wid,
     cell_len_z,app_t,lattice_type,structure_type,out_p,x1,y1,z1,volumeFill,relDenCheck,custom_function)
 
 if strcmp(structure_type,'Solid')                                                           % Check if Solid
-    isovalue = [-1 1]; rd = zeros(size(isovalue));                                          % Initialize isovalue for two extreme values -1 and 1
+    isovalue = [-0.999 0.999]; rd = zeros(size(isovalue));                                          % Initialize isovalue for two extreme values -1 and 1
     for i = 1:length(isovalue)                                                              % for-loop for the two isovalues chosen
         [F,V] = imp_model_func(out_p,x1,y1,z1,cell_len_x,cell_len_y,...
             cell_len_z,app_t,isovalue(i),lattice_type,structure_type,volumeFill,custom_function);
@@ -18,7 +18,7 @@ if strcmp(structure_type,'Solid')                                               
     isovalue_new = (isovalue(2)-isovalue(1))*((rd_in-rd(1))/(rd(2)-rd(1)))+isovalue(1);     % estimate the new isovalue by interpolating between the two initial extreme isovalues
     app_t_new = [];
 else
-    if strcmp(relDenCheck, "Yes")                                                           %check if new approximate thickness is calculated using relative density
+    if relDenCheck == true                                                           %check if new approximate thickness is calculated using relative density
         app_t= [0.2 2];
         rd = zeros(size(app_t));
         for i = 1:length(app_t)
